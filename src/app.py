1d0358b879c9ac4662ea7e4a2ceafd5d4c904402
Muhammad Rehan <muhammadrehans611@gmail.com>
2026-09-11 10:45:54 +0500

    Delete src directory

--- src/app.py
+++
@@ -1,4 +0,0 @@
-print("My AI Engineering journey has started!") 
-print ("I am excited to learn and grow in this field.")
-print("I will work hard to achieve my goals and make a positive impact in the world of AI.")
-print("My AI Engineering journey has started with Git!")
